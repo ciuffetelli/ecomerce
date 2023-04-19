bcfed51207b285642e79070ea3f0c19af70a6acb
Daniel Ciuffetelli <seufetelli@gmail.com>
2023-04-19 19:57:42 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/pages/**/*.tsx",
+    "./src/Components/**/*.tsx",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
