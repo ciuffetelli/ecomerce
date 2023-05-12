440e1a659c81bf3982c90df7f07dbc2e5cb5cd00
Daniel Ciuffetelli <seufetelli@gmail.com>
2023-05-12 19:13:44 +0100

    some fix

--- src/pages/index.public.tsx
+++ src/pages/index.public.tsx
@@ -6,19 +6,6 @@ export default function Home() {
     return (
         <Template>
             <Header />
-            {/* <Header menuData={props.appData.menu} basketData={props.appData.basket}  /> */}
         </Template>
     )
 }
-
-// export async function getStaticProps() {
-
-//     const data = await appData.get()
-
-//     return {
-//         props: {
-//             appData: data ?? {}
-//         },
-//         // revalidate: 600
-//     }
-// }
